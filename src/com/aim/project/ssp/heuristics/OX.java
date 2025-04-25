f3d7913e497609726b0fec921ac7338866f5920c
Taohit <psyta5@nottingham.ac.uk>
2025-04-25 11:38:24 +0100

    implement ox heuristic

--- src/com/aim/project/ssp/heuristics/OX.java
+++ src/com/aim/project/ssp/heuristics/OX.java
@@ -28,26 +28,61 @@ public class OX implements XOHeuristicInterface {
 	@Override
 	public double apply(SSPSolutionInterface solution, double depthOfSearch, double intensityOfMutation) {
 
-		// TODO implementation of Order Crossover
-		return -1.0d;
+		// For single solution case, just copy the solution as per requirements
+		return solution.getObjectiveFunctionValue();
 	}
 
 	@Override
 	public double apply(SSPSolutionInterface p1, SSPSolutionInterface p2,
 						SSPSolutionInterface c, double depthOfSearch, double intensityOfMutation) {
 
-		// TODO implementation of Order Crossover
-		return -1.0d;
+		int[] parent1 = p1.getSolutionRepresentation().getSolutionRepresentation();
+		int[] parent2 = p2.getSolutionRepresentation().getSolutionRepresentation();
+		int length = parent1.length;
+
+		// Select two cut points (ensuring at least one element outside the segment)
+		int cut1 = random.nextInt(length - 1);
+		int cut2;
+		do {
+			cut2 = random.nextInt(length);
+		} while (cut2 <= cut1 || (cut2 - cut1) >= length - 1);
+
+		// Create child solution
+		int[] child = new int[length];
+		boolean[] inSegment = new boolean[length];
+
+		// Copy segment from parent1 to child
+		for (int i = cut1; i <= cut2; i++) {
+			child[i] = parent1[i];
+			inSegment[parent1[i]] = true;
+		}
+
+		// Fill remaining positions from parent2
+		int currentPos = (cut2 + 1) % length;
+		int parent2Pos = (cut2 + 1) % length;
+
+		while (currentPos != cut1) {
+			int gene = parent2[parent2Pos];
+			if (!inSegment[gene]) {
+				child[currentPos] = gene;
+				currentPos = (currentPos + 1) % length;
+			}
+			parent2Pos = (parent2Pos + 1) % length;
+		}
+
+		// Update child solution
+		c.getSolutionRepresentation().setSolutionRepresentation(child);
+		int newValue = f.getObjectiveFunctionValue(c.getSolutionRepresentation());
+		c.setObjectiveFunctionValue(newValue);
+
+		return newValue;
 	}
 
-	/*
-	 * TODO update the methods below to return the correct boolean value.
-	 */
 
 	@Override
 	public boolean isCrossover() {
 
-		return false;
+		return true;
 	}
 
 	@Override
